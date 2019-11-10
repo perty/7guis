28b180e44c790c02912ed8d49055c556fd0b41dc
Per Lundholm <per@artcomputer.se>
2019-11-10 20:28:32 +0100

    Display as a table.

--- elm/cells/src/Main.elm
+++ elm/cells/src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (main)
 import Array
 import Browser
 import Html exposing (div, p, text)
+import Html.Attributes exposing (style, value)
 import Matrix exposing (Matrix)
 
 
@@ -50,6 +51,11 @@ rows =
     List.range 0 99
 
 
+cols : List Int
+cols =
+    List.range 0 24
+
+
 cellInit : Int -> Int -> Cell
 cellInit _ _ =
     Cell EmtpyCell
@@ -74,22 +80,56 @@ view : Model -> Html.Html Msg
 view model =
     div []
         [ p []
-            [ text "Hello world!"
+            [ text "Hello spreadsheet!"
             ]
         , displaySheet model.sheet
         ]
 
 
+tableAttr : List (Html.Attribute Msg)
+tableAttr =
+    [ style "border-style" "solid"
+    , style "border-width" "1px"
+    ]
+
+
+thAttr : List (Html.Attribute Msg)
+thAttr =
+    [ style "border-style" "solid"
+    , style "border-width" "1px"
+    ]
+
+
+tdAttr : List (Html.Attribute Msg)
+tdAttr =
+    [ style "border-style" "solid"
+    , style "border-width" "1px"
+    ]
+
+
+cellAttr : List (Html.Attribute Msg)
+cellAttr =
+    [ style "min-width" "30px"
+    ]
+
+
 displaySheet : Matrix.Matrix Cell -> Html.Html Msg
 displaySheet sheet =
-    Html.table [] <|
-        List.map (displayRow sheet) rows
+    Html.table tableAttr
+        [ Html.thead thAttr ([ Html.th thAttr [] ] ++ List.map col2letter (List.range 0 24))
+        , Html.tbody [] (List.map (displayRow sheet) (List.range 0 99))
+        ]
+
+
+col2letter : Int -> Html.Html Msg
+col2letter n =
+    Html.th thAttr [ Char.fromCode (65 + n) |> String.fromChar |> text ]
 
 
 displayRow : Matrix.Matrix Cell -> Int -> Html.Html Msg
 displayRow sheet row =
     Html.tr []
-        ([ Html.td [] [ text <| String.fromInt row ] ]
+        ([ Html.td tdAttr [ text <| String.fromInt row ] ]
             ++ (Array.map displayCell (Matrix.getYs sheet row)
                     |> Array.toList
                )
@@ -98,7 +138,12 @@ displayRow sheet row =
 
 displayCell : Cell -> Html.Html Msg
 displayCell cell =
-    Html.td [] [ text "cell" ]
+    Html.td cellAttr [ cellContents cell ]
+
+
+cellContents : Cell -> Html.Html Msg
+cellContents cell =
+    Html.input [ value "x" ] []
 
 
 main : Program () Model Msg
