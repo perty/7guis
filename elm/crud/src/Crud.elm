1c13ff448cb8120b7adb07da34e18037259b929d
Per Lundholm <per@artcomputer.se>
2020-10-11 17:03:59 +0200

    Started on the CRUD challenge.

--- elm/crud/src/Crud.elm
+++ elm/crud/src/Crud.elm
@@ -1,35 +1,42 @@
 module Crud exposing (main)
 
 import Browser
-import Html exposing (div, p, text)
-
-
-
--- Starter. Remove this comment when started.
+import Element exposing (column, el, fill, layout, maximum, paddingXY, row, spacingXY, text, width)
+import Element.Border as Border
+import Element.Input as Input
+import Html
 
 
 type Msg
-    = NoOp
+    = UpdatePrefix String
+
+
+type alias Person =
+    { firstName : String
+    , lastName : String
+    }
 
 
 type alias Model =
-    {}
+    { persons : List Person
+    , prefix : String
+    }
 
 
-init : Model
-init =
-    {}
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { persons = [], prefix = "" }, Cmd.none )
 
 
 
 -- Update
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            model
+        UpdatePrefix p ->
+            ( { model | prefix = p }, Cmd.none )
 
 
 
@@ -38,17 +45,25 @@ update msg model =
 
 view : Model -> Html.Html Msg
 view _ =
-    div []
-        [ p []
-            [ text "Hello world!"
+    layout [] <|
+        column
+            [ spacingXY 5 5
+            , paddingXY 20 20
+            , Border.width 1
+            , width (fill |> maximum 300)
             ]
-        ]
+            [ row []
+                [ el [] <| text "Filter prefix:"
+                , Input.text [] {}
+                ]
+            ]
 
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
         , view = view
         , update = update
+        , subscriptions = \_ -> Sub.none
         }
