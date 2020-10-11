4b9e2b8ad7297cf6ec8afcdf33165df92e7fdf97
Per Lundholm <per@artcomputer.se>
2020-10-11 18:45:45 +0200

    Continued on the CRUD challenge.

--- elm/crud/src/Crud.elm
+++ elm/crud/src/Crud.elm
@@ -1,7 +1,7 @@
 module Crud exposing (main)
 
 import Browser
-import Element exposing (column, el, fill, layout, maximum, paddingXY, row, spacingXY, text, width)
+import Element exposing (Element, column, el, fill, height, layout, maximum, paddingXY, row, spacingXY, text, width)
 import Element.Border as Border
 import Element.Input as Input
 import Html
@@ -9,6 +9,9 @@ import Html
 
 type Msg
     = UpdatePrefix String
+    | Create
+    | Update
+    | Delete
 
 
 type alias Person =
@@ -38,25 +41,76 @@ update msg model =
         UpdatePrefix p ->
             ( { model | prefix = p }, Cmd.none )
 
+        Create ->
+            ( model, Cmd.none )
+
 
 
 -- View
 
 
 view : Model -> Html.Html Msg
-view _ =
+view model =
     layout [] <|
         column
             [ spacingXY 5 5
             , paddingXY 20 20
             , Border.width 1
-            , width (fill |> maximum 300)
+            , width (fill |> maximum 500)
             ]
-            [ row []
-                [ el [] <| text "Filter prefix:"
-                , Input.text [] {}
+            [ filterPrefix model
+            , row []
+                [ listPersons model
+                , personView model
                 ]
+            , buttonRow model
             ]
+
+
+filterPrefix : Model -> Element Msg
+filterPrefix model =
+    row []
+        [ Input.text []
+            { onChange = UpdatePrefix
+            , text = model.prefix
+            , placeholder = Nothing
+            , label = Input.labelLeft [] (el [] <| text "Filter prefix:")
+            }
+        ]
+
+
+listPersons model =
+    column [ Border.width 1, width fill, height fill ] (List.map listPerson model.persons)
+
+
+listPerson person =
+    el [] <| text <| person.lastName ++ "," ++ person.firstName
+
+
+personView model =
+    column []
+        [ Input.text []
+            { onChange = UpdatePrefix
+            , text = model.prefix
+            , placeholder = Nothing
+            , label = Input.labelLeft [] (el [] <| text "Name: ")
+            }
+        , Input.text []
+            { onChange = UpdatePrefix
+            , text = model.prefix
+            , placeholder = Nothing
+            , label = Input.labelLeft [] (el [] <| text "Surname: ")
+            }
+        ]
+
+
+buttonRow model =
+    row []
+        [ Input.button []
+            { onPress = Just Create
+            , label = el [] <| text "Create"
+            }
+        ]
 
 
 main : Program () Model Msg
